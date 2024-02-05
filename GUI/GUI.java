9a4c18edd1e4af37965996d478703a65f775a6d3
jcapitanmoreno <jcapitanmoreno@gmail.com>
2024-02-05 09:56:01 +0100

    comit prueba subida a brancj jcapitan

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,5 +1,38 @@
 package GUI;
 
-public class GUI {
+import Interface.Favorito;
 
+public class GUI  implements Interface.GUI {
+
+    @Override
+    public void showMainMenu() {
+        System.out.println("Menu principal");
+        System.out.println("|----------------------------------------------------|");
+        System.out.println("| pulsa una opción valida mostrada a continuación    |");
+        System.out.println("| 1. listar favoritos                                |");
+        System.out.println("| 2. añadir favorito                                 |");
+        System.out.println("| 3. eliminar favorito                               |");
+        System.out.println("| 4. salir del programa                              |");
+        System.out.println("|----------------------------------------------------|");
+    }
+
+    @Override
+    public Favorito showFavorite() {
+        return null;
+    }
+
+    @Override
+    public Favorito addFavorite() {
+        return null;
+    }
+
+    @Override
+    public Favorito removeFavorite() {
+        return null;
+    }
+
+    @Override
+    public void showResult() {
+
+    }
 }
