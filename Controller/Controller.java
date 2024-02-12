b22ae5cddfd438982f36c0a3950f7aca5164b0b9
jcapitanmoreno <jcapitanmoreno@gmail.com>
2024-02-12 18:12:59 +0100

    finalizar controller

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -8,15 +8,17 @@ public class Controller {
     public static Library library = new Library();
 
     public static void startApp() {
-        int option = vista.showMainMenu();
-        mainController(option);
-
+        int option = -1;
+        do {
+            option = vista.showMainMenu();
+            mainController(option);
+        } while (option != 5);
 
 
     }
 
     private static void mainController(int option) {
-        switch (option){
+        switch (option) {
             case 1:
                 selectWhatToSearch();
                 break;
@@ -28,10 +30,9 @@ public class Controller {
                 break;
             case 4:
                 vista.showIfUpdate(library.Update(vista.updateFavorite()));
-
-
-
-
+                break;
+            default:
+                System.out.println("La opcion introducida no se contempla");
 
 
         }
@@ -42,22 +43,22 @@ public class Controller {
                 "1 - Buscar por ID \n" +
                 "2 - Buscar por CLASE \n" +
                 "3 - Buscar por NOMBRE ", 1, 3);
-        switch (option){
+        switch (option) {
             case 1:
                 vista.showFavorite(library.findById(vista.leeString("inserte el identificador")));
                 break;
             case 2:
                 vista.showList(
                         library.findByClass(
-                        vista.solicitateNumber("seleccione la opcion de favorito que desea buscar " +
-                        "peliculas " +
-                        "2 - Musica \n"  +
-                        "3 - Juego",1,3)), "todos los favoritos de la misma clase son: ");
+                                vista.solicitateNumber("seleccione la opcion de favorito que desea buscar " +
+                                        "1 - peliculas \n" +
+                                        "2 - Musica \n" +
+                                        "3 - Juego", 1, 3)), "todos los favoritos de la misma clase son: ");
 
                 break;
             case 3:
                 vista.showList(library.findByName(vista.leeString("inserte el nombre que desea añadir: "))
-                        ,"la lista de coincidencias son:");
+                        , "la lista de coincidencias son:");
                 break;
         }
     }
